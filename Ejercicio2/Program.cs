10933ad7960977d2354b3df305cb1fb75e7702cb
Jorge <jorge.toledo.6@hotmail.com>
2022-09-06 12:36:02 -0300

    VIRTUAL

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ 
+#Identificar y generar los modelos de clases para crear un sistema para administrar las atenciones medicas
+#de una consulta.
+#Es decir, controlar los medicos que trabajan, las horas medicas, a que pacientes atienden, y el detalle de cada consulta
+#que realizan.
+ 
+ */
